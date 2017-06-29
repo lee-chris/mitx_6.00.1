a3fdb0fd5a11694cac28f64f4bc81b20b0a26c9b
Chris Lee <lee.christopher.e@gmail.com>
2017-06-28 19:15:55 -0700

    added implementation for isWordGuessed function

--- week3_problem_set/ps3_hangman.py
+++ week3_problem_set/ps3_hangman.py
@@ -50,7 +50,10 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
+    for l in secretWord:
+        if not l in lettersGuessed:
+            return False
+    return True
 
 
 
