2cbfeabf6e1c23968dd2545e636f17aa89c61944
Chris Lee <lee.christopher.e@gmail.com>
2017-07-04 00:16:53 -0700

    added general_poly function.

--- midterm.py
+++ midterm.py
@@ -83,3 +83,23 @@ def dict_invert(d):
             inverse[val].sort()
     
     return inverse
+
+
+def general_poly (L):
+    """ L, a list of numbers (n0, n1, n2, ... nk)
+    Returns a function, which when applied to a value x, returns the value 
+    n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
+    
+    def fn(x):
+        
+        val = 0
+        k = len(L) - 1
+        
+        for i in L:
+            val += i * x ** k
+            k = k - 1
+        
+        return val
+            
+    return fn
+        
