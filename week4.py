fd8a0d120254a4164f71880d832decdf30644616
Chris Lee <lee.christopher.e@gmail.com>
2017-07-04 19:21:08 -0700

    initial week4 python.
    This initial commit includes the boilerplate code given as part of the
    exercise.

---
+++ week4.py
@@ -0,0 +1,7 @@
+def fancy_divide(list_of_numbers, index):
+   denom = list_of_numbers[index]
+   return [simple_divide(item, denom) for item in list_of_numbers]
+
+
+def simple_divide(item, denom):
+   return item / denom
