8ef0d12149186a52c5a923c827e18cc0d19406c2
Chris Lee <lee.christopher.e@gmail.com>
2017-07-04 19:32:26 -0700

    added try-except to handle ZeroDivisionError properly.

--- week4.py
+++ week4.py
@@ -1,7 +1,17 @@
 def fancy_divide(list_of_numbers, index):
-   denom = list_of_numbers[index]
-   return [simple_divide(item, denom) for item in list_of_numbers]
+    denom = list_of_numbers[index]
+    return [simple_divide(item, denom) for item in list_of_numbers]
 
 
 def simple_divide(item, denom):
-   return item / denom
+    """Divide item by denom.
+    
+    On a division by 0 error, the value 0 is returned.
+    """
+    
+    try:
+        result = item / denom
+    except ZeroDivisionError:
+        return 0
+    else:
+        return result
